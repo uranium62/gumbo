9c41d5b63f69dd2bd83ee01a6ba2046403e4178e
Melnik Alexander <moris912@gmail.com>
2018-10-01 08:51:20 +0300

    init repo

---
+++ src/Standart.Html.Gumbo.Perf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Standart.Html.Gumbo.Perf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Standart.Html.Gumbo.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Standart.Html.Gumbo.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
